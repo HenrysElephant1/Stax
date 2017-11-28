8949599ffc82d8c61690f755253923351f5c4f96
HenrysElephant1 <jacu9594@colorado.edu>
2017-11-28 15:36:04 -0700

    Recreating the STAX directory with new basic webpage files

--- STAX/Pods/Target Support Files/Pods-STAX/Pods-STAX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_STAX : NSObject
-@end
-@implementation PodsDummy_Pods_STAX
-@end
--- STAX/STAX/bridge.m
+++
@@ -1,9 +0,0 @@
-//
-//  bridge.m
-//  STAX
-//
-//  Created by Beltrax on 11/10/17.
-//  Copyright © 2017 TEAM STAX. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
