64f27d0282f9bfa797570aee0c460d9716cd23c7
Yousif Barnawi <yoba0619@colorado.edu>
2017-11-28 12:21:29 -0700

    Update scrap.py

--- WS/scrap.py
+++ WS/scrap.py
@@ -1,5 +1,9 @@
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 #import the library used to query a website
-import urllib2
+
 from bs4 import BeautifulSoup
 # specify the url
 quote_page = 'https://www.target.com/c/slow-cookers-roasters-kitchen-appliances-dining/-/N-5xtrg'
@@ -11,12 +15,12 @@ page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
 
 # Take out the <div> of name and get its value
-name_box = soup.find('span', attrs={'class': 'truncated-title'})
+name_box = soup.find('div', attrs={'class': 'truncated-title'})
 
 name = name_box.text.strip() # strip() is used to remove starting and trailing
 print (name)
 
 # get the index price
-price_box = soup.find('span', attrs={'class':'OnSale'})
+price_box = soup.find('div', attrs={'class':'price'})
 price = price_box.text
 print (price)
