31c9f0220894ee33f047c00c6a41bd7e482dc294
Jason Cummings <jason.cummings@colorado.edu>
2017-12-11 11:13:42 -0700

    Testing on Heroku

--- STAX/favorites.php
+++ STAX/favorites.php
@@ -1,3 +1,20 @@
+<?php 
+
+	// $conn = @mysqli_connect('127.0.0.1', 'root', 'root', 'stax');
+
+	$host = "staxsmysql.mysql.database.azure.com";
+	$db_name = "stax_";
+	$username = "master_stax@staxsmysql";
+	$password = "dev2017softwareB0C@";
+
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+
+	if(mysqli_connect_errno($conn)){
+		die('Failed to connect to MySQL: '.mysqli_connect_error());
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -391,20 +408,6 @@
 
 <?php
 	$DEALS_PER_PAGE = 10;
-
-	// $conn = @mysqli_connect('127.0.0.1', 'root', 'root', 'stax');
-
-	$host = "staxsmysql.mysql.database.azure.com";
-	$db_name = "stax_";
-	$username = "master_stax@staxsmysql";
-	$password = "dev2017softwareB0C@";
-
-	$conn = mysqli_init();
-	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-
-	if(mysqli_connect_errno($conn)){
-		die('Failed to connect to MySQL: '.mysqli_connect_error());
-	}
 
 	$userEmail = $_GET['userName'];
 	if( $sortBy == "oldest" ) {
