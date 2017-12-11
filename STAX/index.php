e596af68cca0a90eb22a4377b7f5540e0db5b558
Jason Cummings <jason.cummings@colorado.edu>
2017-12-10 19:44:27 -0700

    Doing things

--- STAX/index.php
+++ STAX/index.php
@@ -10,38 +10,6 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script>
-		//ASDF
-		var allowVote = true;
-		var allowFavorite = true;
-		function getVotesAndFavorites() {
-			if( USER_EMAIL != "" ) {
-				allowFavorite = false;
-				$.ajax({
-					type: 'POST',
-					url: 'Favorites/getUserFavorites.php',
-					datatype: 'html',
-					data: {userID: USER_EMAIL},
-					success: function(data) {
-						allowFavorite = true;
-						console.log(data);
-					},
-				});
-
-				$.ajax({
-					allowVote = false;
-					type: 'POST',
-					url: 'voting_scripts/getUserVotes.php',
-					datatype: 'html',
-					data: {userID: USER_EMAIL},
-					success: function(data) {
-						allowVote = true;
-						console.log(data);
-					},
-				});
-			}
-		}
-
-		
 		var popMap;
 		var popUpMapMarker;
 		function showPopup( dealID ) {
@@ -239,6 +207,38 @@
 					url: 'Favorites/favorites.php',
 					datatype: 'html',
 					data: {userID: USER_EMAIL, dealID: inputDealID},			
+				});
+			}
+		}
+
+
+		//ASDF
+		var allowVote = true;
+		var allowFavorite = true;
+		function getVotesAndFavorites() {
+			if( USER_EMAIL != "" ) {
+				allowFavorite = false;
+				$.ajax({
+					type: 'POST',
+					url: 'Favorites/getUserFavorites.php',
+					datatype: 'html',
+					data: {userID: USER_EMAIL},
+					success: function(data) {
+						allowFavorite = true;
+						console.log(data);
+					},
+				});
+				
+				allowVote = false;
+				$.ajax({
+					type: 'POST',
+					url: 'voting_scripts/getUserVotes.php',
+					datatype: 'html',
+					data: {userID: USER_EMAIL},
+					success: function(data) {
+						allowVote = true;
+						console.log(data);
+					},
 				});
 			}
 		}
