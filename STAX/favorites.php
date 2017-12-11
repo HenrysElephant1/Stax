47354f67cdf52cf8d7c76ebfa4f25e16ef66fe8e
Jason Cummings <jason.cummings@colorado.edu>
2017-12-11 11:20:15 -0700

    Testing on Heroku

--- STAX/favorites.php
+++ STAX/favorites.php
@@ -192,10 +192,9 @@
 
 	<script type="text/javascript">
 		<?php
-			$totalDealsQuery = "SELECT COUNT(*) FROM favorites WHERE memberID = '".$_GET['userID']."';";
+			$totalDealsQuery = "SELECT COUNT(*) FROM favorites WHERE memberID = '".$_GET['userName']."';";
 			$dealsResultSet = mysqli_query( $conn, $totalDealsQuery );
 			$totalDeals = mysqli_fetch_array( $dealsResultSet )[0];
-			echo $totalDeals;
 			echo "var totalDeals = " . $totalDeals . ";";
 		?>
 
